dbd3f1dff3a4bb9d02a9241481767f1ce3cc9d53
Stephen Peters <7speter@gmail.com>
2022-09-19 10:10:33 -0400

    Adds express.json, sets headers for CORS

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 
+const bookmarkRoutes = require('./routes/bookmarks')
+
 const app = express();
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+app.use(bookmarkRoutes)
 
 app.listen(8080);
