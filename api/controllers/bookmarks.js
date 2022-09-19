4c67b77306f5780fc8693b0793b99026cc6cf502
Stephen Peters <7speter@gmail.com>
2022-09-19 10:11:20 -0400

    creates basic get and post operations

---
+++ api/controllers/bookmarks.js
@@ -0,0 +1,15 @@
+exports.getBookmarks = (req, res, next) => {
+    res.status(200).json({bookmark: "aol.com"})
+}
+
+
+exports.postBookmark = (req, res, next) => {
+    const title = req.body.title;
+    const content =  req.body.content;
+    res.status(201).json({
+        message: 'Post created successfully'
+        //post: {}
+    })
+    console.log(req)
+}
+
