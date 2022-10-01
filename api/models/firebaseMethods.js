ccf5d9946d7c198d5e290e84cbe992f39f56915f
Stephen Peters <7speter@gmail.com>
2022-10-01 01:32:15 -0400

    Creates firebaseMethods in models directory, +s methods

---
+++ api/models/firebaseMethods.js
@@ -0,0 +1,74 @@
+const firebaseAuth = require('./firebase');
+getAuth()
+  .getUser(uid)
+  .then((userRecord) => {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
+  })
+  .catch((error) => {
+    console.log('Error fetching user data:', error);
+  });
+
+/*   getAuth()
+  .createUser({
+    email: 'user@example.com',
+    emailVerified: false,
+    phoneNumber: '+11234567890',
+    password: 'secretPassword',
+    displayName: 'John Doe',
+    photoURL: 'http://www.example.com/12345678/photo.png',
+    disabled: false,
+  })
+  .then((userRecord) => {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log('Successfully created new user:', userRecord.uid);
+  })
+  .catch((error) => {
+    console.log('Error creating new user:', error);
+  });
+ */
+
+  firebaseAuth.getAuth()
+  .createUser({
+    uid: 'some-uid',
+    email: 'user@example.com',
+    phoneNumber: '+11234567890',
+  })
+  .then((userRecord) => {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log('Successfully created new user:', userRecord.uid);
+  })
+  .catch((error) => {
+    console.log('Error creating new user:', error);
+  });
+
+  firebaseAuth.getAuth()
+  .deleteUser(uid)
+  .then(() => {
+    console.log('Successfully deleted user');
+  })
+  .catch((error) => {
+    console.log('Error deleting user:', error);
+  });
+
+
+  
+  firebaseAuth.listAllUsers(nextPageToken) {
+  // List batch of users, 1000 at a time.
+  getAuth()
+    .listUsers(1000, nextPageToken)
+    .then((listUsersResult) => {
+      listUsersResult.users.forEach((userRecord) => {
+        console.log('user', userRecord.toJSON());
+      });
+      if (listUsersResult.pageToken) {
+        // List next batch of users.
+        listAllUsers(listUsersResult.pageToken);
+      }
+    })
+    .catch((error) => {
+      console.log('Error listing users:', error);
+    });
+}
+  // Start listing users from the beginning, 1000 at a time.
+  //listAllUsers();
