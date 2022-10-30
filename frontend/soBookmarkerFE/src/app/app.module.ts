22fb34cfff2556cef4df895711f2ac3d32401592
Stephen Peters <7speter@gmail.com>
2022-10-29 20:36:46 -0400

    imports AngularFire+AngularAuth modules to app.module

--- frontend/soBookmarkerFE/src/app/app.module.ts
+++ frontend/soBookmarkerFE/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
