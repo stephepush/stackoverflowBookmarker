042da970e9ff8083bd613914c6ce1cfd3cda3d45
Stephen Peters <7speter@gmail.com>
2022-10-30 20:08:04 -0400

    💉s AuthService into forgot pword, signin/up, verify 📧 components

--- frontend/soBookmarkerFE/src/app/components/sign-in/sign-in.component.ts
+++ frontend/soBookmarkerFE/src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthService
+  ) { }
+
 
   ngOnInit(): void {
   }
